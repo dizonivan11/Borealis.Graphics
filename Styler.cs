07ff68efa8ad204048ff2ac98ef1d07256968328
Ivan Dizon <dizonivan11@gmail.com>
2018-05-05 00:10:01 +0800

    Added default values for button

--- Styler.cs
+++ Styler.cs
@@ -12,9 +12,15 @@ namespace Borealis.Graphics
             } else {
                 // Load Default Values
                 Add("windowBase", Color.FromNonPremultiplied(100, 125, 200, 150));
-                Add("windowTitle", Color.FromNonPremultiplied(100, 125, 225, 200));
-                Add("windowTitleFore", Color.Black);
+                Add("windowFore", Color.Black);
                 Add("windowBorder", Color.Black);
+                Add("windowTitle", Color.FromNonPremultiplied(100, 125, 225, 200));
+
+                Add("buttonBase", Color.FromNonPremultiplied(100, 125, 200, 225));
+                Add("buttonBaseHover", Color.FromNonPremultiplied(125, 150, 225, 225));
+                Add("buttonBaseActive", Color.FromNonPremultiplied(150, 175, 255, 225));
+                Add("buttonFore", Color.Black);
+                Add("buttonBorder", Color.Black);
             }
         }
 
