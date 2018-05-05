fbaf27c8696b8669e6d3ab0666c08d1d4df3c9a8
Ivan Dizon <dizonivan11@gmail.com>
2018-05-05 10:00:19 +0800

    Fixed visibility issues in window

--- Styler.cs
+++ Styler.cs
@@ -11,10 +11,10 @@ namespace Borealis.Graphics
                 // Add Styles Here From File....
             } else {
                 // Load Default Values
-                Add("windowBase", Color.FromNonPremultiplied(100, 125, 200, 150));
+                Add("windowBase", Color.FromNonPremultiplied(100, 125, 200, 225));
                 Add("windowFore", Color.Black);
                 Add("windowBorder", Color.Black);
-                Add("windowTitle", Color.FromNonPremultiplied(100, 125, 225, 200));
+                Add("windowTitle", Color.FromNonPremultiplied(100, 125, 225, 225));
 
                 Add("buttonBase", Color.FromNonPremultiplied(100, 125, 200, 225));
                 Add("buttonBaseHover", Color.FromNonPremultiplied(125, 150, 225, 225));
