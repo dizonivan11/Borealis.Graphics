4abcdfa629410d3a1afaadc63b732f29d02a62fb
dizonivan11 <dizonivan11@gmail.com>
2018-06-03 00:06:31 +0800

    Texture2D importer added.

---
+++ Importer.cs
@@ -0,0 +1,16 @@
+﻿using Borealis.Graphics.GameObjects;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+
+namespace Borealis.Graphics {
+    public static class Importer {
+        public static Texture2D FromFile(string fileName) {
+            Texture2D ret = null;
+            using (FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read)) {
+                ret = Texture2D.FromStream(GameObject.Graphics.GraphicsDevice, file);
+                file.Close();
+            }
+            return ret;
+        }
+    }
+}
