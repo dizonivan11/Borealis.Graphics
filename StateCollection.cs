beb3108cdad6f41c6b63b239a02fe42b3e29b3b8
dizonivan11 <dizonivan11@gmail.com>
2018-06-04 20:11:14 +0800

    Added StateCollection object to the project

---
+++ StateCollection.cs
@@ -0,0 +1,22 @@
+﻿using Borealis.Graphics.GameObjects;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borealis.Graphics {
+    public class StateCollection : Dictionary<string, Texture2D> {
+        public string CurrentState { get; set; }
+
+        public StateCollection() {
+            CurrentState = "default";
+        }
+
+        public Texture2D GetCurrentState() {
+            if (ContainsKey(CurrentState)) return this[CurrentState];
+            return GameObject.Pixel;
+        }
+    }
+}
